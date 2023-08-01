11f777bb6dca1ea8117d001f8772d773523b6936
parham <parham@SDMM145.local>
2023-08-01 05:10:25 -0700

    Release v0.13.0

--- _nuxt/pricing.24d289f0.js
+++
@@ -1,1 +0,0 @@
-import{aK as a}from"./entry.7726ca09.js";const o=a("pricing",{state:()=>({plans:[],gateway:"shaparak",discountCode:void 0})});export{o as u};
--- _nuxt/useBackground.b28da467.js
+++
@@ -1,1 +0,0 @@
-import{a1 as o}from"./entry.7726ca09.js";function r(a){const e=o("backgroundImageUrl");e.value=a}export{r as u};
